5c13198b5fe34ad9bc4aba18c80f145fbcdc5a4c
Mali Radojica <radojicamali81@gmail.com>
2022-10-02 13:11:09 +0200

    Contact page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -31,4 +31,53 @@ $(document).ready(function () {
             margin: 0
         });
     }
+
+    // form validator
+
+    if ($('.contact-form'.length > 0)) {
+        $('.contact-form').validate({
+            highlight: function (element) {
+                $(element).addClass('is-invalid').removeClass('is-valid');
+            },
+            unhighlight: function (element) {
+                $(element).addClass('is-valid').removeClass('is-invalid');
+            },
+            rules: {
+                name: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                subject: {
+                    required: true
+                },
+                message: {
+                    required: true
+                }
+            },
+            messages: {
+                name: {
+                    required: 'The Name* field is required'
+                },
+                email: {
+                    required: 'The Email* field is required',
+                    email: 'Please provide a valid email address'
+                },
+                subject: {
+                    required: 'The Subject* field is required'
+                },
+                message: {
+                    required: 'The message* field is required'
+                }
+            },
+            errorElement: 'p',
+            errorPlacement: function (error, element) {
+                error.appendTo(element.closest(".form-group").find(".error-msg"));
+            }
+
+        });
+    }
+
 });
